c92a4d49e912c49ec9cabf4051d72032998887d2
OJ De Quintos <2008oliverjesus@gmail.com>
2022-04-19 00:20:16 +0800

    [Task6] Extend the application to create Activities over multiple steps

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: %i[ show edit update destroy ]
+  before_action :current_activity, only: %i[ edit update ]
 
   # GET /activities or /activities.json
   def index
@@ -12,38 +13,38 @@ class ActivitiesController < ApplicationController
 
   # GET /activities/new
   def new
-    @activity = Activity.new
+    session[:activity_params] = {}
+    session[:activity_step] = nil
+    @activity = Activity.new(session[:activity_params])
+    @activity.current_step = session[:activity_step]
   end
 
   # GET /activities/1/edit
   def edit
+    session[:activity_params] = {}
+    session[:activity_step] = nil
+    @activity = Activity.new(session[:activity_params])
+    @activity.current_step = session[:activity_step]
   end
 
   # POST /activities or /activities.json
   def create
-    @activity = Activity.new(activity_params)
+    saving_wizard
 
-    respond_to do |format|
-      if @activity.save
-        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully created." }
-        format.json { render :show, status: :created, location: @activity }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
-      end
+    if @activity.new_record? && !params[:back_button]
+      render "new"
+    else
+      session[:activity_step] = session[:activity_params] = nil
+      flash[:notice] = "Activity was successfully created."
     end
   end
 
   # PATCH/PUT /activities/1 or /activities/1.json
   def update
-    respond_to do |format|
-      if @activity.update(activity_params)
-        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully updated." }
-        format.json { render :show, status: :ok, location: @activity }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
-      end
+    saving_wizard
+
+    if @wizard && !params[:back_button]
+      render "edit"
     end
   end
 
@@ -58,6 +59,46 @@ class ActivitiesController < ApplicationController
   end
 
   private
+
+    def saving_wizard
+      @wizard = true
+      session[:activity_params] ||= {}
+      session[:activity_params].deep_merge!(activity_params) if activity_params
+      @activity = Activity.new(session[:activity_params])
+      @activity.current_step = session[:activity_step]
+
+      if params[:back_button]
+        @activity.current_step = @activity.previous_step
+        session[:activity_step] = @activity.current_step
+        render params[:action] == "update" ?  "edit" : "new" and return
+      end
+
+      if @activity.valid?
+        if @activity.last_step?
+          send("#{params[:action]}_activity") if @activity.all_valid?
+        else
+          @activity.next_step
+        end
+        session[:activity_step] = @activity.current_step
+      end
+    end
+
+    def create_activity
+      @activity.save
+      redirect_to @activity and return
+    end
+
+    def update_activity
+      @current_activity.update(session[:activity_params])
+      @wizard = false
+      redirect_to activities_path and return
+    end
+
+    def current_activity
+      @current_activity = Activity.find(params[:id])
+    end
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_activity
       @activity = Activity.find(params[:id])
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,56 @@
 class Activity < ApplicationRecord
+  attr_writer :current_step
+
+  validate :valid_name?, :if => lambda { |o| o.current_step == "name" }
+  validate :valid_address?, :if => lambda { |o| o.current_step == "address" }
+  validate :valid_time?, :if => lambda { |o| o.current_step == "schedule" }
+  validate :invalid_schedule?, :if => lambda { |o| o.current_step == "schedule" }
+
+  def valid_name?
+    errors.add(:base, 'Please enter valid name') unless name.present?
+  end
+
+  def valid_address?
+    errors.add(:base, 'Please enter valid address') unless address.present?
+  end
+
+  def valid_time?
+    errors.add(:base, 'Please enter valid start_at') unless starts_at.present?
+    errors.add(:base, 'Please enter valid end_at') unless ends_at.present?
+  end
+
+  def invalid_schedule?
+    errors.add(:base, 'Ends At should be ahead Starts At') unless starts_at.present? && ends_at.present? && (starts_at < ends_at)
+  end
+
+  def steps
+    %w[name address schedule]
+  end
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
